75216a5d729a1e432590221af56d6eac44e0a45e
dani <ctrl.alt.d@gmail.com>
2021-08-01 16:35:45 +0200

    WIP. DI

--- BusinessLayer/DI/Injection.cs
+++ BusinessLayer/DI/Injection.cs
@@ -10,10 +10,17 @@ namespace BusinessLayer.DI
         public static void BusinessLayerConfigureServices(this IServiceCollection services)
         {
             // Services (ToDo: per comprensió)
+
+            // centres
             services.AddTransient<ICentres, Centres>();
             services.AddTransient<ICentreCreate, CentreCreate>();
+            services.AddTransient<ICentreUpdate, CentreUpdate>();
+   
+            // alumnes
             services.AddTransient<IAlumnes, Alumnes>();
             services.AddTransient<IAlumneCreate, AlumneCreate>();
+
+            // curs acadèmic
             services.AddTransient<ICursAcademicCreate, CursAcademicCreate>();
         }        
     }
