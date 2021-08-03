1fe95575a5aa26085bbbfbc764b65bc724568058
dani <ctrl.alt.d@gmail.com>
2021-08-03 16:58:34 +0200

    WIP

--- ER.AvaloniaUI/Views/CentresWindow.axaml.cs
+++ ER.AvaloniaUI/Views/CentresWindow.axaml.cs
@@ -1,22 +1,46 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reactive.Concurrency;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-
+using BusinessLayer.Abstract.Services;
+using ER.AvaloniaUI.Services;
+using ReactiveUI;
+using dtoo = DTO.o.DTOs;
 namespace ER.AvaloniaUI.Views
 {
     public partial class CentresWindow : Window
     {
         public CentresWindow()
         {
+
             InitializeComponent();
 #if DEBUG
             this.AttachDevTools();
 #endif
+            
+            RxApp.MainThreadScheduler.Schedule(LoadCentres);
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        public ObservableCollection<dtoo.Centre> MyItems {get;} = new();
+
+        private async void LoadCentres()
+        {
+            var createParms = new DTO.i.DTOs.EsActiuParms(esActiu: true);
+            var l =
+                await
+                AppContext
+                .GetBLOperation<ICentres>()
+                .GetItems(createParms)
+                ;
+
+            l.Data!.ForEach(x=>MyItems.Add(x));
         }
     }
 }
