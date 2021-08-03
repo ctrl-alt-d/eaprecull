14ac808ec862eff5e17d8874428d169af899cfb4
dani <ctrl.alt.d@gmail.com>
2021-08-03 15:58:09 +0200

    Ensure create database

---
+++ CreateDemoData/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using BusinessLayer.Abstract.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+using BusinessLayer.DI;
+using BusinessLayer.Abstract.Generic;
+using DataLayer.DI;
+using System.Threading.Tasks;
+using parms = DTO.i.DTOs;
+
+namespace CreateDemoData
+{
+    class Program
+    {
+        private static ServiceProvider? _ServiceProvider;
+        public static ServiceProvider GetServiceProvider() {
+            _ServiceProvider = _ServiceProvider ??
+                new ServiceCollection()
+                .DataLayerConfigureServices()
+                .BusinessLayerConfigureServices()
+                .BuildServiceProvider();            
+            return _ServiceProvider;
+        }
+
+        public static T GetBLOperation<T>()
+            where T: IBLOperation
+        {
+            return 
+                GetServiceProvider()
+                .GetRequiredService<T>();
+        }
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Creant Centres!");
+            var centreCreate = GetBLOperation<ICentreCreate>();   
+            await centreCreate.Create(new parms.CentreCreateParms("M1", "Les Melies",true));
+            await centreCreate.Create(new parms.CentreCreateParms("M2", "Cendrassos",true));
+        }
+    }
+}
