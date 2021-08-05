946f8a8efb5c4efdd30031a76b2ee10b77dce1c6
dani <ctrl.alt.d@gmail.com>
2021-08-05 09:27:47 +0200

    WIP. centres

---
+++ ER.AvaloniaUI/Pages/CentresUserCtrl.axaml.cs
@@ -0,0 +1,23 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using ER.AvaloniaUI.Services;
+using ER.AvaloniaUI.ViewModels;
+
+namespace ER.AvaloniaUI.Pages {
+    public class CentresUserCtrl : UserControl {
+        public CentresUserCtrl() {
+            InitializeComponent();
+            
+            DataContext = SuperContext.GetViewModel<CentresViewModel>();
+        }
+        
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+    }
+}
