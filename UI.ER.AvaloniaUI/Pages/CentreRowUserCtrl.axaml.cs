2954e5911d6bf01621013b7defbb7c68885d4f25
dani <ctrl.alt.d@gmail.com>
2021-08-16 13:48:43 +0200

    WIP

--- UI.ER.AvaloniaUI/Pages/CentreRowUserCtrl.axaml.cs
+++ UI.ER.AvaloniaUI/Pages/CentreRowUserCtrl.axaml.cs
@@ -10,6 +10,7 @@ using dtoo = DTO.o.DTOs;
 using Avalonia.ReactiveUI;
 using UI.ER.AvaloniaUI.Services;
 using UI.ER.AvaloniaUI.Views;
+using Avalonia.LogicalTree;
 
 namespace UI.ER.AvaloniaUI.Pages
 {
@@ -18,12 +19,24 @@ namespace UI.ER.AvaloniaUI.Pages
         public CentreRowUserCtrl()
         {
             InitializeComponent();
-            this.WhenActivated(d => d(ViewModel!.ShowDialog.RegisterHandler(UpdateShowDialogAsync)));
+            // this.WhenActivated(d => d(ViewModel!.ShowDialog.RegisterHandler(UpdateShowDialogAsync)));
         }
 
         private void InitializeComponent()
         {
-            AvaloniaXamlLoader.Load(this);
+            AvaloniaXamlLoader.Load(this);            
+        }
+
+        protected override void OnDataContextEndUpdate()
+        {
+            base.OnDataContextBeginUpdate();
+            ViewModel?.ShowDialog.RegisterHandler(UpdateShowDialogAsync);
+            
+        }
+
+        protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+        {
+            base.OnAttachedToVisualTree(e);
         }
 
         private async Task UpdateShowDialogAsync(InteractionContext<CentreUpdateViewModel, dtoo.Centre?> interaction)
@@ -34,9 +47,11 @@ namespace UI.ER.AvaloniaUI.Pages
             };
 
             var window = (Window)this.VisualRoot;
+
             var result = await dialog.ShowDialog<dtoo.Centre?>(window);
+
             interaction.SetOutput(result);
-            
+            ( this.Parent as ListBoxItem)!.Focus();
         }
     }
 }
