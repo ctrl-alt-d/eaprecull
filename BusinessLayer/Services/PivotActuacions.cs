016965485c1ca064223e72b17f9b993b2022a035
Dani H <ctrl.alt.d@gmail.com>
2022-07-03 00:50:13 +0200

    Exportar pivot

--- BusinessLayer/Services/PivotActuacions.cs
+++ BusinessLayer/Services/PivotActuacions.cs
@@ -8,31 +8,32 @@ using BusinessLayer.Abstract.Exceptions;
 using BusinessLayer.Abstract.Services;
 using BusinessLayer.Common;
 using DataLayer;
-using DataModels.Models;
 using dtoo = DTO.o.DTOs;
 using Microsoft.EntityFrameworkCore;
-using SharpDocx;
+using OfficeOpenXml;
+using OfficeOpenXml.Table.PivotTable;
 
 namespace BusinessLayer.Services
 {
 
     public class PivotActuacions : BLOperation, IPivotActuacions
     {
+        internal record DataRecord(string Curs, string Alumne, string Data, string Centre, string Tipus, int Minuts, string Etapa, string EstudisObligatoris);
 
         public PivotActuacions(IDbContextFactory<AppDbContext> appDbContextFactory) : base(appDbContextFactory)
         {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
         }
 
         public async Task<OperationResult<dtoo.SaveResult>> Run()
         {
             try
             {
-                var dades = await GetDadesAlumne();
+                var dades = await GetInformes();
 
                 var (path, filename, folder) = CalculaPath("pivot_actuacions");
 
-                // ToDo
-                // Fer el pivot d'actuacions
+                ExportaExcel(path, dades);
 
                 return new(
                     new dtoo.SaveResult(
@@ -57,7 +58,50 @@ namespace BusinessLayer.Services
 
         }
 
-        private Task<List<Actuacio>> GetDadesAlumne()
+        private void ExportaExcel(string path, List<DataRecord> dades)
+        {
+            // https://github.com/EPPlusSoftware/EPPlus/wiki/Pivot-Tables
+            // https://github.com/EPPlusSoftware/EPPlus.Sample.NetCore/blob/master/18-PivotTables/PivotTablesSample.cs
+            using var pck = new ExcelPackage();
+            var wsData=pck.Workbook.Worksheets.Add("Dades");
+            //
+            var dataRange = wsData.Cells["A1"].LoadFromCollection(
+                    dades,
+                    PrintHeaders: true,
+                    TableStyle: OfficeOpenXml.Table.TableStyles.Medium2);
+
+            wsData.Cells[2, 3, dataRange.End.Row, 3].Style.Numberformat.Format = "dd-mm-yy";
+            dataRange.AutoFitColumns();
+
+            //
+            // - pivot
+            //
+            var wsPivot = pck.Workbook.Worksheets.Add("Pivot");
+            var pivotTable1 = wsPivot.PivotTables.Add(wsPivot.Cells["A5"], dataRange, "Actuacions");
+
+            // - page
+            pivotTable1.PageFields.Add(pivotTable1.Fields["Curs"]);
+
+            // - rows
+            pivotTable1.RowFields.Add(pivotTable1.Fields["Centre"]);
+            pivotTable1.RowFields.Add(pivotTable1.Fields["Tipus"]);
+
+            // - data
+            pivotTable1.DataOnRows = false;
+
+            var dataFieldMinuts = pivotTable1.DataFields.Add(pivotTable1.Fields["Minuts"]);
+            dataFieldMinuts.Format="#,##0";
+            dataFieldMinuts.Function = DataFieldFunctions.Sum;
+
+            var dataFieldNombre = pivotTable1.DataFields.Add(pivotTable1.Fields["Minuts"]);
+            dataFieldNombre.Format="#,##0";
+            dataFieldNombre.Function = DataFieldFunctions.Count;
+
+            //
+            pck.SaveAs(new FileInfo(path));
+        }
+
+        private Task<List<DataRecord>> GetInformes()
             =>
             GetContext()
             .Actuacions
@@ -66,13 +110,29 @@ namespace BusinessLayer.Services
             .Include(a => a.CentreAlMomentDeLactuacio)
             .Include(a => a.EtapaAlMomentDeLactuacio)
             .Include(a => a.TipusActuacio)
+            .OrderByDescending(a => a.CursActuacio.AnyInici)
+            .ThenBy(a => a.CentreAlMomentDeLactuacio.Nom)
+            .ThenBy(a => a.Alumne.Cognoms)
+            .ThenBy(a => a.Alumne.Nom)
+            .ThenBy(a => a.MomentDeLactuacio)
             .AsNoTracking()
+            .Select( d => new DataRecord(
+                                d.CursActuacio.Nom,
+                                $"{d.Alumne.Cognoms}, {d.Alumne.Nom} (#{d.Alumne.Id})",
+                                d.MomentDeLactuacio.ToString("yyyy-MM-dd"),
+                                d.CentreAlMomentDeLactuacio.Nom,
+                                d.TipusActuacio.Nom,
+                                d.MinutsDuradaActuacio,
+                                d.EtapaAlMomentDeLactuacio.Nom,
+                                d.EtapaAlMomentDeLactuacio.SonEstudisObligatoris ? "Sí" : "No"
+                )
+            )            
             .ToListAsync();
 
         private static (string path, string filename, string folder) CalculaPath(string prefixfilename)
         {
 
-            var filename = $"{prefixfilename}_{DateTime.Now.ToString("yyyyMMdd-hhmm")}.xslx";
+            var filename = $"{prefixfilename}_{DateTime.Now.ToString("yyyyMMdd-HHmm")}.xlsx";
 
             var binPath = AppDomain.CurrentDomain.BaseDirectory;
 
@@ -93,7 +153,7 @@ namespace BusinessLayer.Services
             {
                 path = Path.Combine(folder, filename);
                 if (!File.Exists(path)) break;
-                filename = filename.Replace(".xslx", "_nou.xslx");
+                filename = filename.Replace(".xlsx", "_nou.xlsx");
             }
 
             return (path, filename, folder);
@@ -121,4 +181,5 @@ namespace BusinessLayer.Services
 
 
     }
+
 }
